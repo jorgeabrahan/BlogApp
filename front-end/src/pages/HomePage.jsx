17baaa9de21653f8fdb64d775b92062228fd04be
jorgeabrahan <jorge24abrahan@gmail.com>
2024-08-07 16:14:31 -0600

    Craete a home page component

---
+++ front-end/src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { MainLayout } from '../layouts';
+
+export const HomePage = () => {
+  return (
+    <MainLayout>
+      <h1>Home</h1>
+    </MainLayout>
+  );
+}
