4db8212ee4f7fe41ee797a99956701cd10c38623
jorgeabrahan <jorge24abrahan@gmail.com>
2024-08-07 16:14:45 -0600

    Craete a profile page component

---
+++ front-end/src/pages/ProfilePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { MainLayout } from '../layouts';
+
+export const ProfilePage = () => {
+  return (
+    <MainLayout>
+      <h1>Profile</h1>
+    </MainLayout>
+  );
+}
