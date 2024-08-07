7b03682eaf77766c38939b7bda5f123d80206ee7
jorgeabrahan <jorge24abrahan@gmail.com>
2024-08-07 16:02:35 -0600

    Create a navbar component with a sign in button and a user dropdown in cased user is logged in

---
+++ front-end/src/components/navbar/Navbar.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { MainActionable } from '../actionables/MainActionable'
+import { MAIN_ACTIONABLE_STYLES } from '../../lib/utils/constants'
+import { SphereIcon } from '../../icons'
+import { UserDropdown } from './UserDropdown'
+import { AuthenticationModal } from './AuthenticationModal'
+
+export const Navbar = () => {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated)
+  const [showAuthenticationModal, setShowAuthenticationModal] = useState(false)
+  return (
+    <section>
+      <nav className='navbar layout-delimiter'>
+        <NavLink to='/' exact className='navbar-logo'>
+          <SphereIcon size='32px' />
+          <p className='navbar-logo__title'>BlogShpere</p>
+        </NavLink>
+
+        {isAuthenticated ? (
+          <UserDropdown />
+        ) : (
+          <MainActionable
+            onClick={() => {
+              setShowAuthenticationModal(true)
+            }}
+            style={MAIN_ACTIONABLE_STYLES.FLAT}
+          >
+            Sign In
+          </MainActionable>
+        )}
+      </nav>
+      <AuthenticationModal
+        isOpen={showAuthenticationModal}
+        setIsOpen={setShowAuthenticationModal}
+      />
+    </section>
+  )
+}
