9c264ba2e37e47ed1667973409802a68861877a5
jorgeabrahan <jorge24abrahan@gmail.com>
2024-08-07 16:12:36 -0600

    Create a barrel file for the components

---
+++ front-end/src/components/index.js
@@ -0,0 +1,9 @@
+// navbar
+export * from './navbar/Navbar'
+
+// routes
+export * from './routes/PrivateRoute';
+
+// actionables
+export * from './actionables/MainActionable';
+export * from './actionables/ServiceActionable';
